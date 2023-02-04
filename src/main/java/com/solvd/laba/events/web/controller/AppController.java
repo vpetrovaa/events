6aa4f5193835a6e781f3567a36e3f5abec7c04f9
vpetrovaa <vpetrova.laba@solvd.com>
2023-02-04 17:44:45 +0300

    Controller basis was added

---
+++ src/main/java/com/solvd/laba/events/web/controller/AppController.java
@@ -0,0 +1,12 @@
+package com.solvd.laba.events.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class AppController {
+
+
+
+}
