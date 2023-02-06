6f88c518728796b7c9e5c1650bc7b50d821ec255
eyakauleva <eyakovleva.laba@solvd.com>
2023-02-06 17:06:52 +0300

    Delete extra exception

--- src/main/java/com/solvd/laba/events/domain/exception/IllegalStatusArgumentException.java
+++
@@ -1,9 +0,0 @@
-package com.solvd.laba.events.domain.exception;
-
-public class IllegalStatusArgumentException extends RuntimeException {
-
-    public IllegalStatusArgumentException(String message) {
-        super(message);
-    }
-
-}
