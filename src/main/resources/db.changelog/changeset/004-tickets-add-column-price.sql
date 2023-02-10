69e6315315268cac1ed9a4c9382df10ab12b1241
vpetrovaa <vpetrova.laba@solvd.com>
2023-02-10 10:37:19 +0300

    Db changelogs were modified

--- src/main/resources/db.changelog/changeset/004-tickets-add-column-price.sql
+++
@@ -1,9 +0,0 @@
---liquibase formatted sql
-
---changeset eyakauleva:tickets_add_column_price
-
-alter table tickets
-    add price decimal(10, 2) not null;
-
--- rollback alter table tickets
---      drop column price;
