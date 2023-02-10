694341d6be31225d134119e8982b7f13081b9c73
vpetrovaa <vpetrova.laba@solvd.com>
2023-02-10 10:45:08 +0300

    Service was modified

--- src/main/java/com/solvd/laba/events/service/TicketService.java
+++ src/main/java/com/solvd/laba/events/service/TicketService.java
@@ -2,12 +2,13 @@ package com.solvd.laba.events.service;
 
 import com.solvd.laba.events.domain.Ticket;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TicketService {
 
     Ticket create(Ticket ticket);
 
-    List<Ticket> findAllByUserId(Long id);
+    List<Ticket> findAll(Long userId, LocalDateTime currentDate);
 
 }
