7d4880a36ff96c3777b0b23ac5401525efcabbd6
vpetrovaa <vpetrova.laba@solvd.com>
2023-02-23 20:26:17 +0300

    K8s config was changed

---
+++ up.sh
@@ -0,0 +1,13 @@
+cd src/infra/
+
+kubectl apply -f pg-secrets.yml
+kubectl apply -f pg-stateful.yml
+kubectl apply -f pg-service.yml
+kubectl apply -f minio-secrets.yml
+kubectl apply -f minio-secrets.yml
+kubectl apply -f minio-stateful.yml
+kubectl apply -f minio-service.yml
+kubectl apply -f events-secrets.yml
+kubectl apply -f events-configmap.yml
+kubectl apply -f events-deployment.yml
+kubectl apply -f events-service.yml
